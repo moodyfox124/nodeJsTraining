e55e4fe740dff7accb17765dd227681084e1e793
moodyfox124 <moodyfox124@gmail.com>
2020-10-12 01:53:26 +0300

    Added homework6

--- mainProject/index.ts
+++ mainProject/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import userRoutes from "./routes/users";
 import groupRouter from "./routes/groups";
+import authenticationRouter from "./routes/authentication";
 import { logger } from "./utils/logger/loggerInstance";
 import { requestLogger } from "./helpers/middlewares/requestLogger";
 import { errorHandler } from "./helpers/middlewares/errorHandler";
+import { jwtVerification } from "./helpers/middlewares/jwtVerifivation";
+import cors from "cors";
+import { corsOptionsDelegate } from "./helpers/cors";
+const PORT = process.env.PORT || 3000;
 
 process.on("uncaughtException", (err) => {
   logger.error(err.message);
@@ -11,17 +16,25 @@ process.on("uncaughtException", (err) => {
 });
 
 process.on("unhandledRejection", (err) => {
-  logger.error(err.message);
+  logger.error(err);
   process.exit(1);
 });
 
 const app = express();
-const PORT = 3000;
+app.disable("x-powered-by");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
+app.use(cors(corsOptionsDelegate));
+
+app.use("/authentication", authenticationRouter);
+
+app.use(jwtVerification);
+
 app.use("/users", userRoutes);
 app.use("/groups", groupRouter);
+
 app.use(errorHandler);
 
 app.listen(PORT, () => {
